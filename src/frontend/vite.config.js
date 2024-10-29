47fc87f5d07960efdaad5182d603a6ad22367a71
Evren Yeniev <mail@evren.dev>
2024-10-29 19:00:44 +0100

    Refactor package.json to add vite-svg-loader dependency

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,18 +1,46 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueDevTools from "vite-plugin-vue-devtools"
+import svgLoader from "vite-svg-loader"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), svgLoader()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 3000,
+    hot: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "static/chunks/[name]-[hash].js",
+        entryFileNames: "static/entries/[name]-[hash].js",
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+            return "assets/images/[name]-[hash][extname]"
+          }
+
+          if (/\.css$/.test(name ?? "")) {
+            return `assets/css/${name
+              .split(/\.?(?=[A-Z])/)
+              .join("-")
+              .toLowerCase()}-[hash][extname]`
+          }
+
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return "assets/[name]-[hash][extname]"
+        },
+      },
+    },
+    sourcemap: false,
+    target: "esnext",
+  },
 })
