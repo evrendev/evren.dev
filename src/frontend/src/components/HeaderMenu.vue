63fca9d1d5f4a6cf152d6587c7b993e4a37b9883
Evren Yeniev <mail@evren.dev>
2024-10-31 12:12:23 +0100

    Update HeaderMenu component with new logo and enhanced styling

--- src/frontend/src/components/HeaderMenu.vue
+++ src/frontend/src/components/HeaderMenu.vue
@@ -2,9 +2,9 @@
 
 <template>
   <div class="page_header">
-    <div class="logo transition_link" data-type="image">
+    <div class="logo transition_link" data-type="text">
       <a href="#home">
-        <h3>TRAMP</h3>
+        <h3>EVREN</h3>
       </a>
     </div>
     <div class="menu">
@@ -19,4 +19,54 @@
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page_header {
+  position: fixed;
+  top: 0px;
+  right: 70px;
+  left: 70px;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 11;
+  padding: 0px 60px;
+
+  .logo {
+    img {
+      max-width: 130px;
+    }
+
+    h3 {
+      font-family: "Montserrat";
+      font-weight: 900;
+      font-size: 31px;
+      letter-spacing: 5px;
+    }
+  }
+  .menu {
+    ul {
+      display: flex;
+      align-items: center;
+
+      li {
+        margin-right: 40px;
+
+        a {
+          color: var(--bc);
+          transition: all 0.3s ease;
+        }
+
+        &:last-child {
+          margin-right: 0px;
+        }
+
+        &.active a,
+        a:hover {
+          color: #000;
+        }
+      }
+    }
+  }
+}
+</style>
