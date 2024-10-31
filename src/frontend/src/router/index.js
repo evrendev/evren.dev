064a9595f47f6fb162235221fc76f90f5048c527
Evren Yeniev <mail@evren.dev>
2024-10-31 13:04:03 +0100

    Add ContactView component and update router to include contact route

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -15,6 +15,11 @@ const router = createRouter({
       name: "about",
       component: () => import("../views/AboutView.vue"),
     },
+    {
+      path: "/contact",
+      name: "contact",
+      component: () => import("../views/ContactView.vue"),
+    },
   ],
 })
 
---
+++ src/frontend/src/views/ContactView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="contact">
+    <span>Contact</span>
+  </div>
+</template>
