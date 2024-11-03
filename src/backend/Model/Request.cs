9667bdb62d2cd42bd9a57928bc95b85accedeefa
Evren Yeniev <mail@evren.dev>
2024-11-03 21:04:05 +0100

    Add Request model to handle contact form data

---
+++ src/backend/Model/Request.cs
@@ -0,0 +1,11 @@
+namespace EvrenDev.Model;
+
+public class Request
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Subject { get; set; }
+    public string? Message { get; set; }
+    public string? RecaptchaToken { get; set; }
+}
