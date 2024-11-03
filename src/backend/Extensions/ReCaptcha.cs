d89b4075d23cc28f3222d515bf7027347af18dd0
Evren Yeniev <mail@evren.dev>
2024-11-03 21:04:27 +0100

    Add ReCaptcha validation and configuration to SendMailApi

---
+++ src/backend/Extensions/ReCaptcha.cs
@@ -0,0 +1,40 @@
+using Newtonsoft.Json.Linq;
+
+namespace EvrenDev.Extensions;
+
+public class ReCaptcha
+{
+    private readonly HttpClient _captchaClient;
+
+    private readonly IConfiguration _configuration;
+
+    private readonly ILogger<ReCaptcha> _logger;
+
+    public ReCaptcha(HttpClient captchaClient,
+        IConfiguration configuration,
+        ILogger<ReCaptcha> logger)
+    {
+        _captchaClient = captchaClient;
+        _configuration = configuration;
+        _logger = logger;
+    }
+
+    public async Task<bool> IsValid(string token)
+    {
+        try
+        {
+            string? secretKey = _configuration.GetSection("Recaptcha.SecretKey").Get<string>();
+            HttpResponseMessage postTask = await _captchaClient
+                .PostAsync($"?secret={secretKey}&response={token}", new StringContent(""));
+            string result = await postTask.Content.ReadAsStringAsync();
+            JObject? resultObject = JObject.Parse(result);
+            dynamic success = resultObject["success"] ?? false;
+            return (bool)success;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+            return false;
+        }
+    }
+}
