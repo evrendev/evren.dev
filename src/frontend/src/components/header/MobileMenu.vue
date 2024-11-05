5b84dd64fcfd87b22057dd27ce61e1bac5a8eb71
Evren Yeniev <mail@evren.dev>
2024-11-05 11:41:25 +0100

    Refactor mobile header: replace existing implementation with MobileTopbar and create separate MobileMenu component for improved structure and maintainability

---
+++ src/frontend/src/components/header/MobileMenu.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { MenuItems } from "./"
+import { EMail, SocialMediaAccounts } from "../shared/"
+</script>
+
+<template>
+  <div class="mobile-menu" :class="{ opened: isOpened }">
+    <menu-items class="menu-items" />
+    <div class="extra">
+      <social-media-accounts style="margin-bottom: 5px" />
+      <EMail />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.mobile-menu {
+  position: fixed;
+  top: 50px;
+  right: -200px;
+  height: 100vh;
+  width: 200px;
+  z-index: 15;
+  background-color: #fff;
+
+  -webkit-transition: all 0.3s ease;
+  -moz-transition: all 0.3s ease;
+  -ms-transition: all 0.3s ease;
+  -o-transition: all 0.3s ease;
+  transition: all 0.3s ease;
+
+  &.opened {
+    right: 0px;
+  }
+
+  .menu-items {
+    width: 100%;
+    height: auto;
+    text-align: right;
+    padding: 100px 20px 0px 20px;
+  }
+
+  .extra {
+    width: 100%;
+    text-align: right;
+    padding: 0px 20px;
+    margin-top: 50px;
+  }
+}
+</style>
---
+++ src/frontend/src/components/header/MobileTopbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="page_topbar">
+    <div class="topbar_inner">
+      <text-logo />
+      <mobile-menu-button @click="toggleMenu" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+import { TextLogo, MobileMenuButton } from "./"
+
+const isOpened = ref(false)
+
+const toggleMenu = () => {
+  isOpened.value = !isOpened.value
+}
+</script>
+
+<style lang="scss" scoped>
+.page_topbar {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  height: 50px;
+  background-color: #fff;
+  z-index: 14;
+  display: none;
+
+  @media (max-width: 1024px) {
+    display: block;
+  }
+
+  .topbar_inner {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px 20px;
+  }
+}
+</style>
