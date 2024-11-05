17ef87f2b75b1a59cbdaaf11bca606f57f739419
Evren Yeniev <mail@evren.dev>
2024-11-05 10:02:29 +0100

    Add Accept-Language header to Axios instance for localization support

--- src/frontend/src/plugins/axios.js
+++ src/frontend/src/plugins/axios.js
@@ -1,9 +1,14 @@
 import axios from "axios"
+import Tr from "@/plugins/i18n/translation"
 
 const baseUrl = `${import.meta.env.VITE_API_URL}`
+const acceptLanguage = Tr.currentLocale
 const instance = axios.create({
   baseURL: baseUrl,
-  header: { "Content-Type": "application/json" },
+  header: {
+    "Content-Type": "application/json",
+    "Accept-Language": acceptLanguage,
+  },
 })
 
 export default instance
