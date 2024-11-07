4126af02833858dcf75ea3d7146cce17b1ed4659
Evren Yeniev <mail@evren.dev>
2024-11-07 10:07:04 +0100

    Add biography section and CV download functionality to About page

--- src/frontend/src/views/about/DownloadCvButton.vue
+++ src/frontend/src/views/about/DownloadCvButton.vue
@@ -1,6 +1,22 @@
 <script setup>
+import "@fancyapps/ui/dist/fancybox/fancybox.css"
+import { Fancybox } from "@fancyapps/ui"
+import { ref, watch } from "vue"
 import { useI18n } from "vue-i18n"
+import { useRoute } from "vue-router"
+
 const { t } = useI18n()
+
+const route = useRoute()
+const currentLanguage = ref(route.params.locale)
+watch(
+  () => route.params.locale,
+  async value => (currentLanguage.value = value),
+)
+
+Fancybox.bind("[data-fancybox]", {
+  l10n: currentLanguage.value,
+})
 
 const pdfFileSrc = new URL(
   "@/assets/documents/Evren_Yeniev_CV.pdf",
@@ -10,7 +26,7 @@ const pdfFileSrc = new URL(
 
 <template>
   <div class="page-button">
-    <a :href="pdfFileSrc" target="_blank" class="text_effect"
+    <a :href="pdfFileSrc" class="text_effect" data-fancybox
       ><span class="wrapper">
         <span class="before">{{ t("page.about.biography.download-cv") }}</span
         ><span class="after">{{
