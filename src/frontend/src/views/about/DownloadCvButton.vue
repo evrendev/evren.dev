3dd24e66f50611599775aa8e62417f5dc2e260f6
Evren Yeniev <mail@evren.dev>
2025-10-09 11:40:07 +0200

    Refactor DownloadCvButton component to enhance language handling and PDF file selection

--- src/frontend/src/views/about/DownloadCvButton.vue
+++ src/frontend/src/views/about/DownloadCvButton.vue
@@ -1,38 +1,50 @@
+// DownloadButton.vue
+
 <script setup>
 import "@fancyapps/ui/dist/fancybox/fancybox.css"
 import { Fancybox } from "@fancyapps/ui"
-import { ref, watch } from "vue"
+import { computed, watch, onMounted } from "vue"
 import { useI18n } from "vue-i18n"
-import { useRoute } from "vue-router"
 
-const { t } = useI18n()
+import enPdf from "@/assets/documents/Evren_Yeniev_Resume_en.pdf"
+import dePdf from "@/assets/documents/Evren_Yeniev_Resume_de.pdf"
 
-const route = useRoute()
-const currentLanguage = ref(route.params.locale)
-watch(
-  () => route.params.locale,
-  async value => (currentLanguage.value = value),
-)
+const { t, locale } = useI18n()
 
-Fancybox.bind("[data-fancybox]", {
-  l10n: currentLanguage.value,
+const pdfFiles = {
+  en: enPdf,
+  de: dePdf,
+}
+
+const pdfFileSrc = computed(() => {
+  return pdfFiles[locale.value] || pdfFiles.en
 })
 
-const pdfFileSrc = new URL(
-  "@/assets/documents/Evren_Yeniev_CV.pdf",
-  import.meta.url,
-).href
+watch(
+  () => locale.value,
+  newLocale => {
+    Fancybox.destroy()
+    Fancybox.bind("[data-fancybox]", {
+      l10n: newLocale,
+    })
+  },
+  {},
+)
+
+onMounted(() => {
+  Fancybox.bind("[data-fancybox]", {
+    l10n: locale.value,
+  })
+})
 </script>
 
 <template>
   <div class="page-button">
-    <a :href="pdfFileSrc" class="text-effect" data-fancybox
-      ><span class="wrapper">
-        <span class="before">{{ t("page.about.biography.download-cv") }}</span
-        ><span class="after">{{
-          t("page.about.biography.download-cv")
-        }}</span></span
-      ></a
-    >
+    <a :href="pdfFileSrc" class="text-effect" data-fancybox>
+      <span class="wrapper">
+        <span class="before">{{ t("page.about.biography.download-cv") }}</span>
+        <span class="after">{{ t("page.about.biography.download-cv") }}</span>
+      </span>
+    </a>
   </div>
 </template>
